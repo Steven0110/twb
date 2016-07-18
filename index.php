ba19edd28580167e1e3b570219a0a8033b3626e5
Steven0110 <cabello.acosta.gerardo@gmail.com>
2016-07-18 18:58:55 -0500

    18-julio

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.com/libraries/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <!-- Mis CSS -->
+    <link rel="stylesheet" href="css/blocks.css" />
+    <link rel="stylesheet" href="css/buttons.css" />
+
+    <title>Metal Pizza</title>
+
+</head>
+
+<body>
+    <div id="header">
+        <div id="saludo" class="header-izq col-md-10">
+            Buenos días Steven
+        </div>
+        <div class="header-der col-md-2">
+            Ordenar ahora
+        </div>
+    </div>
+    <div class="logo">
+
+    </div>
+    <div class="nav">
+        <button class="nav-btn nav-btn-left col-md-2 col-sm-2 col-xs-2 col-lg-2">INICIO</button>
+        <button class="nav-btn col-md-2 col-sm-2 col-xs-2 col-lg-2">MENÚ</button>
+        <button class="nav-btn col-md-2 col-sm-2 col-xs-2 col-lg-2">PROMOCIONES</button>
+        <button class="nav-btn col-md-2 col-sm-2 col-xs-2 col-lg-2">SUCURSALES</button>
+        <button class="nav-btn col-md-2 col-sm-2 col-xs-2 col-lg-2">LOG IN</button>
+        <button class="nav-btn nav-btn-right col-md-2 col-sm-2 col-xs-2 col-lg-2">CONTÁCTANOS</button>
+    </div>
+    <div class="container">
+
+    </div>
+</body>
+
+</html>
