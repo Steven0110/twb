a43458b74f30e22691c106910985b63ea0fdd22c
Steven0110 <cabello.acosta.gerardo@gmail.com>
2016-07-21 03:49:12 -0500

    21 julio

--- js/map-contact.js
+++ js/map-contact.js
@@ -10,7 +10,7 @@ function init(){
     });
     marker.setMap( map );
     var info = new google.maps.InfoWindow({
-        content: "<div class='info-content'><p class='info-title'>OFICINAS CENTRALES</p><p class='map-title'>DIRECCIÓN: </p>Filadelfia 124 Nápoles 03810 Ciudad de México, D.F.<p class='map-title'>TELÉFONO: </p>55-31305867<p class='info-title'>REDES SOCIALES</p><div class='icons-bar'><a href='#'><img src='img/other/fb-icon.png' class='icon' /></a><a href='#'><img src='img/other/twitter-icon.png' class='icon' /></a><a href='#'><img src='img/other/yt-icon.png' class='icon' /></a></div></div>",
+        content: "<div class='info-content'><p class='info-title'>OFICINAS CENTRALES</p><p class='map-title'>DIRECCIÓN: </p>Filadelfia 124 Nápoles 03810 Ciudad de México, D.F.<p class='map-title'>TELÉFONO: </p>55-31305867<p class='map-title'>E-MAIL: </p>cabello.acosta.gerardo@gmail.com<p class='info-title'>REDES SOCIALES</p><div class='icons-bar'><a href='#'><img src='img/other/fb-icon.png' class='icon' /></a><a href='#'><img src='img/other/twitter-icon.png' class='icon' /></a><a href='#'><img src='img/other/yt-icon.png' class='icon' /></a></div></div>",
     });
     info.open( map, marker);
     marker.addListener("click", function(){
